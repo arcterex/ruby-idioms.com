99f90b8f9500e640637c865736651a03f97aec99
Alan Bailward <alan@ufies.org>
2010-06-14 22:33:27 -0700

    Added new idiom in controller

--- app/controllers/idioms_controller.rb
+++ app/controllers/idioms_controller.rb
@@ -6,12 +6,24 @@ class IdiomsController < ApplicationController
   def new
     @idiom = Idiom.new
   end
+  
   def show
     @idiom = Idiom.find(params[:id])
   end
 
   def create
-    @idiom = Idiom.new
+    @idiom = Idiom.new(params[:idiom])
+    if @idiom.save then
+      flash[:notice] = "Idiom was created"
+      redirect_to idioms_path
+    else
+      flash[:error] = "Error saving your idiom"
+      render :action => "edit"
+    end
+  end
+  
+  def edit
+    @idiom = Idiom.find(params[:id])
   end
 
   def update
